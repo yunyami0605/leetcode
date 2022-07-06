99ea74204be0f4adb5b0b46e2bd05959b72d2492
YOON\dbswh <yunyami0605@gmail.com>
2022-07-06 19:00:50 +0900

    1. a724 problem solve

---
+++ 2_LeetCode75/a724.js
@@ -0,0 +1,47 @@
+/**
+* title : 724. Find Pivot Index
+* Runtime :  112 ms
+* Memory :  45.1 MB
+* Description: 
+    Given an array of integers nums, calculate the pivot index of this array.
+
+    The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+    If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+    Return the leftmost pivot index. If no such index exists, return -1.
+* Constraints: 
+    
+* example
+    Input: nums = [1,7,3,6,5,6]
+    Output: 3
+    Explanation:
+    The pivot index is 3.
+    Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+    Right sum = nums[4] + nums[5] = 5 + 6 = 11
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  let total = nums.reduce((acc, cur) => acc + cur);
+  let leftSum = 0;
+
+  for (let pivot = 0; pivot < nums.length; pivot++) {
+    total -= nums[pivot];
+
+    if (total == leftSum) {
+      return pivot;
+    }
+    leftSum += nums[pivot];
+  }
+
+  return -1;
+};
+// 28
+// 27 0
+// 20 1
+// 17 8
+// 11 11
